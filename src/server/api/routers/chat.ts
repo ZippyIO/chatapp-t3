ba03c6635fd5cad2491ced057c73deb09a18c528
Nick <delta2888@gmail.com>
2023-04-13 14:00:28 +1000

    ✨ feat: add chat router with create procedure

---
+++ src/server/api/routers/chat.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const chatRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(z.object({ content: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.message.create({
+        data: {
+          content: input.content,
+          authorId: ctx.session.user.id,
+        },
+      });
+    }),
+});
